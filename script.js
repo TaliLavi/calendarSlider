91d067e65504160246da52330e5fff853bb36e5c
RoisinOk <roisin_ok@hotmail.com>
2015-08-21 14:31:05 +0100

    Can move infinitely through slides. When next or prev button is pressed, new slides are appended or destroyed, depending on direction. Margin moves so that the slides remain visible as scrolled through.

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ var nextSlideNumber = 1;
 var prevSlideNumber = -1;
 
 jQuery(document).ready(function ($) {
-    //var toChange = $('#wrapper').find('div:nth-child(3)');
-    //toChange.innerHTML="Number 3!";
-    var slideIndex = INITIAL_SLIDE;
 
     var slideCount = $('.week').length;
     var slideWidth = $('.week').width();
@@ -21,99 +18,75 @@ jQuery(document).ready(function ($) {
     // make the wrapper wide enough to accommodate all the weeks,
     // and shift wrapper according to INITIAL_SLIDE
     $('#wrapper').css({width: sliderUlWidth, marginLeft: -(INITIAL_SLIDE - 1) * slideWidth});
+    var wrap = document.getElementById('wrapper'),
+        style = window.getComputedStyle(wrap),
+        marginLeft = style.getPropertyValue('margin-left');
+    //console.log("Margin Left Wrapper"+ marginLeft);
 
-    function moveSlide(slideIndex) {
-        // move wrapper according to new slide's id
-        $('#wrapper').animate({left: -(slideIndex  - INITIAL_SLIDE) * slideWidth}, SLIDE_DURATION);
+    function moveSlideRight() {
+        //get current left position of wrapper
+        var element = document.getElementById('wrapper'),
+            style = window.getComputedStyle(element),
+            left = style.getPropertyValue('left');
+        //parse into number
+        var parsedLeft = parseInt(left);
+        console.log("Wrapper Left Position : "+ parsedLeft);
+        console.log("Wrapper left position minus 600px : "+ (parsedLeft - slideWidth));
+        //take 600 away from current left position, and this is the position it needs to animate to
+        $('#wrapper').animate({left: (parsedLeft-slideWidth)}, SLIDE_DURATION );
+        var wrap = document.getElementById('wrapper'),
+            style = window.getComputedStyle(wrap),
+            marginLeft = style.getPropertyValue('margin-left');
+        //parse into number
+        var parsedMarginLeft = parseInt(marginLeft);
+        console.log("Margin Left Wrapper : "+ parsedMarginLeft);
+        console.log("Margin left plus 600px : "+ (parsedMarginLeft + slideWidth));
+        $('#wrapper').css({width: sliderUlWidth, marginLeft: (parsedMarginLeft + slideWidth)});
+    };//end of moveSlide function
+
+    function moveSlideLeft() {
+        //get current left position of wrapper
+        var element = document.getElementById('wrapper'),
+            style = window.getComputedStyle(element),
+            left = style.getPropertyValue('left');
+        //parse into number
+        var parsedLeft = parseInt(left);
+        console.log("Wrapper Left Position : "+ parsedLeft);
+        console.log("Wrapper left position plus 600px : "+ (parsedLeft + slideWidth));
+        //add 600 to current left position, and this is the position it needs to animate to
+        $('#wrapper').animate({left: (parsedLeft + slideWidth)}, SLIDE_DURATION);
+        var wrap = document.getElementById('wrapper'),
+            style = window.getComputedStyle(wrap),
+            marginLeft = style.getPropertyValue('margin-left');
+        //parse into number
+        var parsedMarginLeft = parseInt(marginLeft);
+        console.log("Margin Left Wrapper : "+ parsedMarginLeft);
+        console.log("Margin left minus 600px : "+ (parsedMarginLeft - slideWidth));
+        $('#wrapper').css({width: sliderUlWidth, marginLeft: (parsedMarginLeft - slideWidth)});
     };//end of moveSlide function
 
 
     $('a.control_prev').click(function () {
-        // as long as current slide's index is within bounds (greater than 1), allow moving left
-        console.log("Initial slide Count:" + slideCount);
-        console.log("Initial Slide Index:" +slideIndex);
         var slideHTML = "<div class = 'week'>"+ prevSlideNumber +"</div>";
-        $("#wrapper").prepend(slideHTML);
-        prevSlideNumber --;
-        //Now need to remove the first div
         var toRemove = $('#wrapper').find('div').last();
         toRemove.remove();
-        console.log("New slide Count:" + slideCount);
-        slideIndex -= 1;
-        console.log("New Slide Index:" +slideIndex);
-        moveSlide(slideIndex);
-        slideIndex += 1;
-        console.log("Final Slide Index:" +slideIndex);
+        $("#wrapper").prepend(slideHTML);
+        prevSlideNumber --;
+        moveSlideLeft();
     });//end of prev click function
 
     $('a.control_next').click(function () {
-        console.log("Initial slide Count:" + slideCount);
-        console.log("Initial Slide Index:" +slideIndex);
         var slideHTML = "<div class = 'week'>"+ nextSlideNumber +"</div>";
-        $("#wrapper").append(slideHTML);
-        nextSlideNumber ++;
-        //Now need to remove the first div
         var toRemove = $('#wrapper').find('div').first();
         toRemove.remove();
-        console.log("New slide Count:" + slideCount);
-        slideIndex += 1;
-        console.log("New Slide Index:" +slideIndex);
-        moveSlide(slideIndex);
-        slideIndex -= 1;
-        console.log("Final Slide Index:" +slideIndex);
+        $("#wrapper").append(slideHTML);
+        nextSlideNumber ++;
+        moveSlideRight();
     });//end of next click function
 
 });//end of document.ready function
 
 
-    //When the next button is pressed, a new slide is appended to the
-    //end of the slides, and the first slide is removed, so that the number
-    //of slides remains the same. (Still a problem with slide index
-    //increasing each time, so that when the time the slide index is 6,
-    //there is no slide visible).
-
-//$('a.control_next').click(function () {
-//    console.log("Slide Count:" +slideCount);
-//    console.log("Initial Slide Index:" +slideIndex);
-//    //slideIndex ++;
-//    console.log("Slide Index after increase:" +slideIndex);
-//
-//    if(slideIndex == (slideCount-2)){
-//
-//        var slideHTML = "<div class = 'week'></div>";
-//        $("#wrapper").append(slideHTML);
-//        //Now need to remove the first div
-//        var toRemove = $('#wrapper').find('div').first();
-//        toRemove.remove();
-//        moveSlide(slideIndex);
-//        slideIndex --;
-//        console.log("Slide Index after move:" +slideIndex);
-//
-//    } else if(slideIndex == (slideCount-1)){
-//
-//        var slideHTML = "<div class = 'week'></div>"
-//        $("#wrapper").append(slideHTML);
-//        //Now need to remove the first div
-//        var toRemove = $('#wrapper').find('div').first();
-//        toRemove.remove();
-//        moveSlide(slideIndex)
-//        slideIndex --;
-//        console.log("Slide Index after move:" +slideIndex);
-//
-//    } else {
-//
-//        var slideHTML = "<div class = 'week'></div>"
-//        $("#wrapper").append(slideHTML);
-//        //Now need to remove the first div
-//        var toRemove = $('#wrapper').find('div').first();
-//        toRemove.remove();
-//        moveSlide(slideIndex);
-//        slideIndex --;
-//        console.log("Slide Index after move:" +slideIndex);
-//    }
-//});
-//
-//});
 
 
 
